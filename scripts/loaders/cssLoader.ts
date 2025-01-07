87513ecdebf18451e99ebd8f212262180f95ffab
uiovasot <esp32chess@gmail.com>
2025-01-07 18:40:56 +0900

    add clean-css

--- scripts/loaders/cssLoader.ts
+++ scripts/loaders/cssLoader.ts
@@ -1,6 +1,9 @@
 import type {BunPlugin} from 'bun';
 import fs from 'fs/promises';
 import {addCSS} from '../../src/utils/addCSS';
+import CleanCSS from 'clean-css';
+
+const cleanCSS = new CleanCSS({});
 
 export default {
     name: 'css-loader',
@@ -8,11 +11,12 @@ export default {
         build.onLoad({filter: /\.css$/}, async (args) => {
             const cssContent = await fs.readFile(args.path, 'utf-8');
 
+            const css = cleanCSS.minify(cssContent);
+
             return {
-                contents: `var addCSS = ${addCSS.toString()};addCSS(${JSON.stringify(cssContent)});`,
+                contents: `var addCSS = ${addCSS.toString()};addCSS(${JSON.stringify(css.styles)});`,
                 loader: 'js',
             };
         });
     },
 } as BunPlugin;
-
